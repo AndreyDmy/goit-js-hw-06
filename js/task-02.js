87288d1124133098f14e07f1de2977b2d9ddc247
AndreyDmy <bananza2772@gmail.com>
2021-12-19 20:23:25 +0200

    goit-js-hw-06

--- js/task-02.js
+++ js/task-02.js
@@ -6,11 +6,25 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const refList = document.querySelector("#ingredients");
 
-for (let i = 0; i < ingredients.length; i += 1) {
-  const refEl = document.createElement("li");
-  refEl.textContent = ingredients[i];
-  refEl.classList.add("item");
-  refList.append(refEl);
-}
+// const refList = document.querySelector("#ingredients")
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const refEl = document.createElement("li");
+//   refEl.textContent = ingredients[i];
+//   refEl.classList.add("item");
+//   refList.append(refEl);
+// }
+
+
+const ingredientsRef = document.querySelector("#ingredients")
+const makeIngredientsList = (elementList) => {
+  return elementList.map((ingredientName) => {
+    const ingredItemRef = document.createElement("li");
+    ingredItemRef.textContent = ingredientName;
+    ingredItemRef.classList.add("item");
+    return ingredItemRef;
+  });
+};
+
+const ingredientsList = makeIngredientsList(ingredients);
+ingredientsRef.append(...ingredientsList);
